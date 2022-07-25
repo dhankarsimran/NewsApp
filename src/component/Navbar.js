621dd02958a2357e8e51f1e862cdfe901132247f
Simran Dhankar <83290371+dhankarsimran@users.noreply.github.com>
2022-07-25 20:08:34 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,19 +1,13 @@
 import React from "react";
-import Profile from "./Profile";
 
+import { Link } from "react-router-dom";
 const Navbar = (props) => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            <img
-              src="http://adgitmdelhi.ac.in/wp-content/uploads/2018/07/logo-adgitm-small2.jpg"
-              width={65}
-              height={54}
-              alt='LOGO'
-              style={{ borderRadius: "50%", border: "solid white 3px" }}
-            />
+            [VB]News
           </a>
           <button
             className="navbar-toggler"
@@ -35,36 +29,41 @@ const Navbar = (props) => {
               </li>
 
               <li className="nav-item">
-                <a className="nav-link" href="/about">
-                  About
+                <a className="nav-link" href="/business">
+                  Business
                 </a>
               </li>
-
+              <li className="nav-item">
+                <a className="nav-link" href="/entertainment">
+                  Entertainment
+                </a>
+              </li>
               <li className="nav-item">
                 <a className="nav-link" href="/general">
                   General
                 </a>
               </li>
-
+              <li className="nav-item">
+                <a className="nav-link" href="/health">
+                  Health
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/science">
+                  Science
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/sports">
+                  Sports
+                </a>
+              </li>
               <li className="nav-item">
                 <a className="nav-link" href="/technology">
                   Technology
                 </a>
               </li>
             </ul>
-            <form className="d-flex" style={{marginRight:'100px'}}>
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                style={{width:'600px'}}
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-             <Profile/>
           </div>
         </div>
       </nav>
@@ -73,78 +72,3 @@ const Navbar = (props) => {
 };
 
 export default Navbar;
-
-// import React from "react";
-
-// import { Link } from "react-router-dom";
-// const Navbar = (props) => {
-//   return (
-//     <div>
-//       <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-//         <div className="container-fluid">
-//           <a className="navbar-brand" href="/">
-//             [VB]News
-//           </a>
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarSupportedContent"
-//             aria-controls="navbarSupportedContent"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <a className="nav-link active" aria-current="page" href="/">
-//                   Home
-//                 </a>
-//               </li>
-
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/business">
-//                   Business
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/entertainment">
-//                   Entertainment
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/general">
-//                   General
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/health">
-//                   Health
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/science">
-//                   Science
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/sports">
-//                   Sports
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="/technology">
-//                   Technology
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
